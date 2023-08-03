df9d10fc16d8cd18bbd160385b4f632da2224764
Tolgaycode <tolgaycode@gmail.com>
2023-08-03 17:13:50 +0300

    update

--- script.js
+++ script.js
@@ -2,6 +2,43 @@ const amountInput = document.querySelector(".amount-input");
 const priceInput = document.querySelector(".price-input");
 const totalSpentInput = document.querySelector(".total-spent-input");
 
+//Portfolio select
+const portfolioListItems = document.querySelectorAll(".protfolio-list li");
+
+const portfolioListArray = Array.from(portfolioListItems);
+
+let protfolioSelected = "Protfolio 1";
+
+portfolioListArray.forEach((item) => {
+  item.addEventListener("click", () => {
+    protfolioSelected = item.querySelector("span").textContent;
+    console.log(protfolioSelected);
+  });
+});
+
+// .add-protfolio button
+const addPortfolioButton = document.querySelector(".add-protfolio");
+addPortfolioButton.addEventListener("click", () => {
+  const userInput = prompt("Lütfen yeni bir portföy seçeneği girin:");
+  if (userInput) {
+    const portfolioList = document.querySelector(".protfolio-list");
+    const newOption = document.createElement("li");
+    newOption.innerHTML = `
+      <div class="wallet">
+        <i class="fa-solid fa-wallet" style="color: #ffffff"></i>
+        <span>${userInput}</span>
+      </div>
+    `;
+    portfolioList.appendChild(newOption);
+
+    // Yeni öğeye tıklama olayı ekleyelim
+    newOption.addEventListener("click", () => {
+      protfolioSelected = userInput;
+      console.log("Seçilen Portföy:", protfolioSelected);
+    });
+  }
+});
+
 // Add Transaction Total spent JS
 // amountInput ve priceInput alanlarının her değiştiğinde güncellemeleri dinleyin
 amountInput.addEventListener("input", updateTotalSpent);

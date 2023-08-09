5c4504786c3556c1c7d1240be58ea6122babdd84
Tolgaycode <tolgaycode@gmail.com>
2023-08-09 12:03:37 +0300

    update

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//////////////////////////////////////////////////////////////////////
-const amountInput = document.querySelector(".amount-input");
-const priceInput = document.querySelector(".price-input");
-const totalSpentInput = document.querySelector(".total-spent-input");
-
 //Portfolio select
 const portfolioListItems = document.querySelectorAll(".protfolio-list li");
 
@@ -44,12 +39,20 @@ addPortfolioButton.addEventListener("click", () => {
   }
 });
 
+const protfolioList = document.querySelector(".protfolio-list");
+
+protfolioList.addEventListener("click", () => {
+  const listTransactionContainer = document.querySelector(".list-transaction");
+  listTransactionContainer.innerHTML = "";
+  let updatedData = JSON.parse(localStorage.getItem(protfolioSelected)) || [];
+  renderTransactionList(updatedData);
+});
+
 // Portfolio name change webpage
 var protfolioName = document.querySelector(".protfolio-name");
 
 function updatePortfolioText() {
   protfolioName.textContent = protfolioSelected;
-  console.log("Seçilen updated Portföy:", protfolioSelected);
 }
 updatePortfolioText();
 
@@ -58,6 +61,11 @@ updatePortfolioText();
 //// ADD TRANSACTION JS
 
 // add transaction amountInput ve priceInput alanlarının her değiştiğinde güncellemeleri dinleyin
+
+const amountInput = document.querySelector(".amount-input");
+const priceInput = document.querySelector(".price-input");
+const totalSpentInput = document.querySelector(".total-spent-input");
+
 amountInput.addEventListener("input", updateTotalSpent);
 priceInput.addEventListener("input", updateTotalSpent);
 
@@ -70,7 +78,7 @@ function updateTotalSpent() {
   totalSpentInput.textContent = `$ ${totalSpent.toFixed(2)}`;
 }
 
-// .transaction-btn active
+// .transaction-btn window open active
 let transactionOpenBtn = document.querySelector(".transaction-btn");
 let overlay = document.querySelector(".overlay");
 
@@ -104,7 +112,6 @@ const sellSelect = document.querySelector(".sell-btn");
 const transferSelect = document.querySelector(".transfer-btn");
 
 let typeInput = "";
-console.log(`Selected Type: ${typeInput}` + "(typeInput)");
 
 buySelect.addEventListener("click", function () {
   typeInput = "Buy";
@@ -136,6 +143,153 @@ const currentMinute = String(now.getMinutes()).padStart(2, "0");
 timeInput.value = `${currentHour}:${currentMinute}`;
 
 const selectedDate = today;
-console.log("Seçilen tarih: " + selectedDate + "(selectedDate)");
 const selectedTime = timeInput.value;
-console.log("Seçilen zaman: " + selectedTime + "(selectedTime)");
+
+/////////////////////////////////////////////////////////
+const addTransactionBtn = document.querySelector(".add-transaction-btn");
+
+addTransactionBtn.addEventListener("click", () => {
+  const transactionType = typeInput;
+  const coinName = document.querySelector(".coin-input").value;
+  const amount = document.querySelector(".amount-input").value;
+  const price = document.querySelector(".price-input").value;
+  const date = selectedDate;
+  const time = selectedTime;
+  const totalSpent = totalSpentInput.textContent.slice(2);
+
+  // Inputların doldurulması ve Local ekleme
+  const selectedPortfolioKey = `${protfolioSelected}`;
+  const transactionData = {
+    type: transactionType,
+    name: coinName,
+    amount: amount,
+    price: price,
+    date: date,
+    time: time,
+    value: totalSpent,
+  };
+  console.log(selectedPortfolioKey);
+  console.log(transactionData);
+
+  let saveData = JSON.parse(localStorage.getItem(protfolioSelected)) || [];
+  saveData.push(transactionData);
+  localStorage.setItem(selectedPortfolioKey, JSON.stringify(saveData));
+  let updatedData = JSON.parse(localStorage.getItem(protfolioSelected));
+  renderTransactionList(updatedData);
+});
+
+// Local deki veriyi html ekleme
+window.addEventListener("load", function () {
+  let savedData = JSON.parse(localStorage.getItem(protfolioSelected)) || [];
+  renderTransactionList(savedData);
+});
+
+function renderTransactionList(transactionData) {
+  const listTransactionContainer = document.querySelector(".list-transaction");
+
+  listTransactionContainer.innerHTML = "";
+
+  transactionData.forEach((transaction, index, editIndex) => {
+    const listItem = document.createElement("div");
+    listItem.classList.add("list-item");
+
+    const transType = document.createElement("div");
+    transType.classList.add("trans-type");
+    transType.innerHTML = `<div class=${transaction.type.toLowerCase()}>${
+      transaction.type
+    }</div>`;
+
+    const transDate = document.createElement("div");
+    transDate.classList.add("trans-date");
+    transDate.textContent = transaction.date + " " + transaction.time;
+
+    const transName = document.createElement("div");
+    transName.classList.add("trans-name");
+    transName.innerHTML = `
+      <i class="fa-solid fa-litecoin-sign" style="color: #ffffff"></i>
+      <div class="coin">${transaction.name}</div>
+    `;
+
+    const transAmount = document.createElement("div");
+    transAmount.classList.add("trans-amount");
+    transAmount.textContent = transaction.amount;
+
+    const transPrice = document.createElement("div");
+    transPrice.classList.add("trans-price");
+    transPrice.textContent = transaction.price;
+
+    const transValue = document.createElement("div");
+    transValue.classList.add("trans-value");
+    transValue.textContent = transaction.value;
+
+    const transPnl = document.createElement("div");
+    transPnl.classList.add("trans-pnl");
+    transPnl.textContent = "pnl $00.00";
+
+    const transAction = document.createElement("div");
+    transAction.classList.add("trans-action");
+    const penToSquare = document.createElement("i");
+    penToSquare.classList.add("fa-regular", "fa-pen-to-square");
+    penToSquare.style.color = "#ffffff";
+    const trashCan = document.createElement("i");
+    trashCan.classList.add("fa-regular", "fa-trash-can");
+    trashCan.style.color = "#ffffff";
+
+    transAction.appendChild(penToSquare);
+    transAction.appendChild(trashCan);
+
+    // transAction.innerHTML = `
+    //   <i class="fa-regular fa-pen-to-square" style="color: #ffffff"></i>
+    //   <i class="fa-regular fa-trash-can" style="color: #ffffff"></i>
+    // `;
+
+    listItem.appendChild(transType);
+    listItem.appendChild(transDate);
+    listItem.appendChild(transName);
+    listItem.appendChild(transAmount);
+    listItem.appendChild(transPrice);
+    listItem.appendChild(transValue);
+    listItem.appendChild(transPnl);
+    listItem.appendChild(transAction);
+
+    listTransactionContainer.appendChild(listItem);
+
+    penToSquare.addEventListener("click", () => {
+      editTransaction(index);
+    });
+    trashCan.addEventListener("click", () => {
+      deleteEntry(index);
+    });
+  });
+}
+
+// Edit transaction faction TAMAMLANMADI
+function editTransaction(index) {
+  // overlay.style.display = "flex";
+  // let updatedData = JSON.parse(localStorage.getItem(protfolioSelected)) || [];
+  // updatedData.splice(index, 1);
+  // let coinNameEdit = document.querySelector(".coin-input");
+  // coinNameEdit.value = updatedData[0].name;
+  // let amountEdit = document.querySelector(".amount-input");
+  // amountEdit.value = updatedData[0].amount;
+  // console.log(updatedData[0]);
+  // console.log(updatedData[0].amount);
+  // console.log(index.amount);
+}
+
+// Delete transaction faction
+function deleteEntry(index) {
+  let deleteData = JSON.parse(localStorage.getItem(protfolioSelected)) || [];
+  deleteData.splice(index, 1);
+  localStorage.setItem(protfolioSelected, JSON.stringify(deleteData));
+  renderTransactions();
+  console.log(index);
+}
+
+// Render Transactions faction
+function renderTransactions() {
+  const listTransactionContainer = document.querySelector(".list-transaction");
+  listTransactionContainer.innerHTML = "";
+  let updatedData = JSON.parse(localStorage.getItem(protfolioSelected)) || [];
+  renderTransactionList(updatedData);
+}
